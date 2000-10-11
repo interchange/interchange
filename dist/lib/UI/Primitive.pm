90a4232d495daa55b0639f6d6c9c5f6fcde60f34
Mike Heins <mike@perusion.com>
2000-10-11 14:38:46 +0000

    Added "--none--" or other default options for tables, columns, filters. Pulled append/prepend out of imagedir type to make it apply to all types.

--- dist/lib/UI/Primitive.pm
+++ dist/lib/UI/Primitive.pm
@@ -23,7 +23,7 @@ my($order, $label, %terms) = @_;
 
 package UI::Primitive;
 
-$VERSION = substr(q$Revision: 1.22 $, 10);
+$VERSION = substr(q$Revision: 1.23 $, 10);
 $DEBUG = 0;
 
 use vars qw!
@@ -746,7 +746,7 @@ sub meta_display {
 #::logDebug("passed = '$passed'");
 
 				if($passed eq 'tables') {
-					$record->{passed} = list_tables({ joiner => ',' });
+					$record->{passed} = "=--none--," . list_tables({ joiner => ',' });
 				}
 				elsif($passed eq 'filters') {
 					$record->{passed} = $Vend::Interpolate::Tag->filters(1),
@@ -755,14 +755,14 @@ sub meta_display {
 					my $total = $1;
 					my $tname = $2 || $record->{db} || $table;
 					$tname = $base_entry_value if $total eq '::';
-::logDebug("tname='$tname' total=$total");
+#::logDebug("tname='$tname' total=$total");
 					my $db = $Vend::Database{$tname};
-					$record->{passed} = join (',', $db->columns())
+					$record->{passed} = join (',', "=--none--", $db->columns())
 						if $db;
 				}
 				elsif($passed =~ /^keys(::(\w+))?$/) {
 					my $tname = $2 || $record->{db} || $table;
-					$record->{passed} = list_keys($tname, { joiner => ',' });
+					$record->{passed} = "=--none--," . list_keys($tname, { joiner => ',' });
 				}
 			}
 		}
@@ -822,21 +822,17 @@ sub meta_display {
 			$record->{type} = 'combo';
 			$record->{passed} = join ",",
 									map { s/,/&#44;/g; $_} @files;
-			$record->{append} = Vend::Util::resolve_links($record->{append})
-				and $record->{append} =~ s/_UI_VALUE_/$value/g
-				and $record->{append} =~ s/_UI_TABLE_/$table/g
-				and $record->{append} =~ s/_UI_COLUMN_/$column/g
-				and $record->{append} =~ s/_UI_KEY_/$key/g
-				if $record->{append};
-			
-			$record->{prepend} = Vend::Util::resolve_links($record->{prepend})
-				and $record->{prepend} =~ s/_UI_VALUE_/$value/g
-				and $record->{prepend} =~ s/_UI_TABLE_/$table/g
-				and $record->{prepend} =~ s/_UI_COLUMN_/$column/g
-				and $record->{prepend} =~ s/_UI_KEY_/$key/g
-				if $record->{prepend};
 		}
 
+		for(qw/append prepend/) {
+			next unless $record->{$_};
+			$record->{$_} = Vend::Util::resolve_links($record->{$_});
+			$record->{$_} =~ s/_UI_VALUE_/$value/g;
+			$record->{$_} =~ s/_UI_TABLE_/$table/g;
+			$record->{$_} =~ s/_UI_COLUMN_/$column/g;
+			$record->{$_} =~ s/_UI_KEY_/$key/g;
+		}
+			
 		if($record->{height}) {
 			if($record->{type} =~ /multi/i) {
 				$record->{type} = "MULTIPLE SIZE=$record->{height}";
