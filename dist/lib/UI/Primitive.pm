6d97bf4ef543bf4626f8c9fdb921c051e33620e2
Mike Heins <mike@perusion.com>
2000-10-12 04:11:41 +0000

    Feed through height/width from tag if present, probably should just make a clean sweep.

--- dist/lib/UI/Primitive.pm
+++ dist/lib/UI/Primitive.pm
@@ -23,7 +23,7 @@ my($order, $label, %terms) = @_;
 
 package UI::Primitive;
 
-$VERSION = substr(q$Revision: 1.23 $, 10);
+$VERSION = substr(q$Revision: 1.24 $, 10);
 $DEBUG = 0;
 
 use vars qw!
@@ -832,7 +832,10 @@ sub meta_display {
 			$record->{$_} =~ s/_UI_COLUMN_/$column/g;
 			$record->{$_} =~ s/_UI_KEY_/$key/g;
 		}
-			
+		for(qw/height width/) {
+			$record->{$_} = $o->{$_}
+				if defined $o->{$_};
+		}
 		if($record->{height}) {
 			if($record->{type} =~ /multi/i) {
 				$record->{type} = "MULTIPLE SIZE=$record->{height}";
