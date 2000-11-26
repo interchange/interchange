d70907f40d810a207abe4344836c767b98305bcc
Stefan Hornburg <racke@linuxia.de>
2000-11-26 10:01:45 +0000

    merged changes 1.24 vs 1.26

--- lib/Vend/Config.pm
+++ lib/Vend/Config.pm
@@ -1,6 +1,6 @@
 # Config.pm - Configure Interchange
 #
-# $Id: Config.pm,v 1.23.4.4 2000-10-24 18:25:12 racke Exp $
+# $Id: Config.pm,v 1.23.4.5 2000-11-26 10:01:45 racke Exp $
 #
 # Copyright (C) 1996-2000 Akopia, Inc. <info@akopia.com>
 #
@@ -89,6 +89,7 @@ use strict;
 use vars qw(
 			$VERSION $C $CanTie
 			@Locale_directives_ary @Locale_directives_scalar
+			@Locale_directives_code
 			@Locale_directives_currency @Locale_keys_currency
             %global_ignore
            );
@@ -104,7 +105,7 @@ BEGIN {
 	};
 }
 
-$VERSION = substr(q$Revision: 1.23.4.4 $, 10);
+$VERSION = substr(q$Revision: 1.23.4.5 $, 10);
 
 my %CDname;
 
@@ -170,6 +171,18 @@ qw/
 	UseModifier
 	AutoModifier
 /   );
+
+# These are extra routines that are run if certain directives are
+# updated
+# Form:
+#
+# [ 'Directive', \&routine, [ @args ] ],
+# 
+# @args are optional.
+# 
+@Locale_directives_code = (
+	[ 'ProductFiles', \&Vend::Data::update_productbase ],
+);
 
 my %DumpSource = (qw(
 					SpecialPage			1
@@ -2143,7 +2156,7 @@ sub parse_database {
 	my($database,$remain) = split /[\s,]+/, $value, 2;
 
 	if( ! defined $c->{$database} ) {
-		$c->{$database} = { 'name' => $database };
+		$c->{$database} = { 'name' => $database, included_from => $configfile };
 		$new = 1;
 	}
 
