ff0b7f7e65a3bb094a3fbd140d411d8e79d7ae1b
Jon Jensen <jon@endpoint.com>
2001-02-22 00:02:24 +0000

    Fixed typo.

--- lib/Vend/MakeCat.pm
+++ lib/Vend/MakeCat.pm
@@ -2,7 +2,7 @@
 #
 # MakeCat.pm - routines for catalog configurator
 #
-# $Id: MakeCat.pm,v 1.12.6.1 2000-12-13 16:18:30 zarko Exp $
+# $Id: MakeCat.pm,v 1.12.6.2 2001-02-22 00:02:24 jon Exp $
 #
 # Copyright (C) 1996-2000 Akopia, Inc. <info@akopia.com>
 #
@@ -62,7 +62,7 @@ sethistory
 use strict;
 
 use vars qw($Force $Error $History $VERSION);
-$VERSION = substr(q$Revision: 1.12.6.1 $, 10);
+$VERSION = substr(q$Revision: 1.12.6.2 $, 10);
 
 $Force = 0;
 $History = 0;
@@ -106,7 +106,7 @@ my %Desc = (
 #                         ^^^^^^^^^^^^^^^^
 #
 # We set it to the name of the catalog by default to enable the
-# internal HTTTP server.
+# internal HTTP server.
 #
 EOF
 	basedir    =>  <<EOF,
