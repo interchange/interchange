37256511c51ac5a7039f32e2af1727a95ac3a94e
Mike Heins <mike@perusion.com>
2001-03-06 17:24:51 +0000

    * Fix problem with CyberCash remapping on route-by-route basis.     Thanks to nferrari@ccsc.com for the sharp eyes.

--- lib/Vend/Order.pm
+++ lib/Vend/Order.pm
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# $Id: Order.pm,v 1.18.2.10 2001-02-22 19:59:53 heins Exp $
+# $Id: Order.pm,v 1.18.2.11 2001-03-06 17:24:51 heins Exp $
 #
 # Copyright (C) 1996-2000 Akopia, Inc. <info@akopia.com>
 #
@@ -31,7 +31,7 @@
 package Vend::Order;
 require Exporter;
 
-$VERSION = substr(q$Revision: 1.18.2.10 $, 10);
+$VERSION = substr(q$Revision: 1.18.2.11 $, 10);
 
 @ISA = qw(Exporter);
 
@@ -1649,7 +1649,7 @@ sub route_order {
 			};
 			for(@vars) {
 				next unless exists $glob->{$_};
-				$::Variable->{$_} = $glob->{_};
+				$::Variable->{$_} = $glob->{$_};
 			}
 			$CGI::values{mv_cyber_mode} = $save;
 			unless ($ok) {
