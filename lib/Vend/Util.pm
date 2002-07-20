a697fa132ea24ac760922fdf8a7a8b24120282b3
Mike Heins <mike@perusion.com>
2002-07-20 19:35:11 +0000

    * Fix bad previous patch.

--- lib/Vend/Util.pm
+++ lib/Vend/Util.pm
@@ -1,6 +1,6 @@
 # Vend::Util - Interchange utility functions
 #
-# $Id: Util.pm,v 2.29 2002-07-20 18:59:33 mheins Exp $
+# $Id: Util.pm,v 2.30 2002-07-20 19:35:11 mheins Exp $
 # 
 # Copyright (C) 1996-2002 Red Hat, Inc. <interchange@redhat.com>
 #
@@ -82,7 +82,7 @@ require HTML::Entities;
 use Safe;
 use subs qw(logError logGlobal);
 use vars qw($VERSION @EXPORT @EXPORT_OK);
-$VERSION = substr(q$Revision: 2.29 $, 10);
+$VERSION = substr(q$Revision: 2.30 $, 10);
 
 BEGIN {
 	eval {
@@ -713,7 +713,7 @@ sub logData {
 
 	$file = ">>$file" unless $file =~ /^[|>]/;
 
-	my $msg = tabbed @msg if @msg > 1;
+	my $msg = @msg > 1 ? tabbed(@msg) : $msg[0];
 
     eval {
 		unless($file =~ s/^[|]\s*//) {
