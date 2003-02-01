397f31b1bdf83b79773d4ef709f3b6bd52c5be4f
Mike Heins <mike@perusion.com>
2003-02-01 21:45:16 +0000

    * Fix "double March" bug in date widget.

--- lib/Vend/Form.pm
+++ lib/Vend/Form.pm
@@ -1,6 +1,6 @@
 # Vend::Form - Generate Form widgets
 # 
-# $Id: Form.pm,v 2.23 2002-12-12 03:18:57 mheins Exp $
+# $Id: Form.pm,v 2.24 2003-02-01 21:45:16 mheins Exp $
 #
 # Copyright (C) 1996-2002 Red Hat, Inc. <interchange@redhat.com>
 #
@@ -37,7 +37,7 @@ use vars qw/@ISA @EXPORT @EXPORT_OK $VERSION %Template/;
 require Exporter;
 @ISA = qw(Exporter);
 
-$VERSION = substr(q$Revision: 2.23 $, 10);
+$VERSION = substr(q$Revision: 2.24 $, 10);
 
 @EXPORT = qw (
 	display
@@ -326,7 +326,7 @@ INITTIME: {
 
 	for(1 .. 12) {
 		$t[4] = $_ - 1;
-		$t[5] = 1;
+		$t[3] = 1;
 		push @Months, [sprintf("%02d", $_), POSIX::strftime("%B", @t)];
 	}
 
