ae37568bea3d472fa8e1c0607ff22dc015e04007
Stefan Hornburg <racke@linuxia.de>
2003-03-21 16:37:31 +0000

    added _map_array method, call it from each_nokey

--- lib/Vend/Table/Shadow.pm
+++ lib/Vend/Table/Shadow.pm
@@ -1,6 +1,6 @@
 # Vend::Table::Shadow - Access a virtual "Shadow" table
 #
-# $Id: Shadow.pm,v 1.22 2003-02-17 12:18:16 racke Exp $
+# $Id: Shadow.pm,v 1.23 2003-03-21 16:37:31 racke Exp $
 #
 # Copyright (C) 2002-2003 Stefan Hornburg (Racke) <racke@linuxia.de>
 #
@@ -20,7 +20,7 @@
 # MA  02111-1307  USA.
 
 package Vend::Table::Shadow;
-$VERSION = substr(q$Revision: 1.22 $, 10);
+$VERSION = substr(q$Revision: 1.23 $, 10);
 
 # TODO
 #
@@ -238,8 +238,12 @@ sub each_record {
 
 sub each_nokey {
 	my ($s, $qual) = @_;
+	my $record;
+	
 	$s = $s->import_db() unless defined $s->[$OBJ];
-	return $s->[$OBJ]->each_nokey($qual);
+	if ($record = $s->[$OBJ]->each_nokey($qual)) {
+		return $s->_map_array ($record);
+	}
 }
 
 sub reset {
@@ -281,6 +285,18 @@ sub _map_hash {
 	}
 
 	$href;
+}
+
+sub _map_array {
+	my ($s, $aref) = @_;
+	my (@cols) = $s->columns();
+	my $key = $aref->[0];
+	
+	for (my $i = 1; $i < @cols; $i++) {
+		$aref->[$i] = $s->_map_column ($key, $cols[$i], 1, $aref->[$i]);
+	}
+
+	$aref;
 }
 
 sub _map_column {
