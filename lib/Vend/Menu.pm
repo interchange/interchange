88f10a0c82b480f28dee115d54a0894e2563f7e8
Stefan Hornburg <racke@linuxia.de>
2003-11-14 14:32:58 +0000

    allow translation of explode tree / collapse tree in menus

--- lib/Vend/Menu.pm
+++ lib/Vend/Menu.pm
@@ -1,6 +1,6 @@
 # Vend::Menu - Interchange menu processing routines
 #
-# $Id: Menu.pm,v 2.34 2003-11-07 18:28:53 mheins Exp $
+# $Id: Menu.pm,v 2.35 2003-11-14 14:32:58 racke Exp $
 #
 # Copyright (C) 2002 Mike Heins, <mike@perusion.net>
 #
@@ -21,7 +21,7 @@
 
 package Vend::Menu;
 
-$VERSION = substr(q$Revision: 2.34 $, 10);
+$VERSION = substr(q$Revision: 2.35 $, 10);
 
 use Vend::Util;
 use strict;
@@ -396,10 +396,13 @@ sub old_tree {
 		$opt->{collapse_url} .= 'collapse=1';
 	}
 
+	my $explode_label = errmsg($opt->{explode_label} || 'Explode tree');
+	my $collapse_label = errmsg($opt->{collapse_label} || 'Collapse tree');
+
 	$opt->{header_template} ||= <<EOF;
 <P>
-<a href="{EXPLODE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>Explode tree</A><br>
-<a href="{COLLAPSE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>Collapse tree</A>
+<a href="{EXPLODE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>$explode_label</A><br>
+<a href="{COLLAPSE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>$collapse_label</A>
 </P>
 EOF
 
@@ -855,10 +858,12 @@ sub dhtml_tree {
 	$opt->{toggle_class} ||= '';
 	$opt->{explode_url} ||= "javascript:${vpf}do_explode(); void(0)";
 	$opt->{collapse_url} ||= "javascript:${vpf}do_collapse(); void(0)";
+	my $explode_label = errmsg($opt->{explode_label} || 'Explode tree');
+	my $collapse_label = errmsg($opt->{collapse_label} || 'Collapse tree');
 	$opt->{header_template} ||= <<EOF;
 <P>
-<a href="{EXPLODE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>Explode tree</A><br>
-<a href="{COLLAPSE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>Collapse tree</A>
+<a href="{EXPLODE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>$explode_label</A><br>
+<a href="{COLLAPSE_URL}" {LINK_STYLE?} style="{LINK_STYLE}"{/LINK_STYLE?} {LINK_CLASS?} class="{LINK_CLASS}"{/LINK_CLASS?}>$collapse_label</A>
 </P>
 EOF
 
