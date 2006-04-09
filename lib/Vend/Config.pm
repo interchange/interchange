6af8215173277893f86f13d6b78b094e4adca03a
Davor Ocelic <docelic@spinlocksolutions.com>
2006-04-09 23:27:53 +0000

    * Get rid of three unused config directives

--- lib/Vend/Config.pm
+++ lib/Vend/Config.pm
@@ -1,6 +1,6 @@
 # Vend::Config - Configure Interchange
 #
-# $Id: Config.pm,v 2.204 2006-04-05 08:01:58 racke Exp $
+# $Id: Config.pm,v 2.205 2006-04-09 23:27:53 docelic Exp $
 #
 # Copyright (C) 2002-2006 Interchange Development Group
 # Copyright (C) 1996-2002 Red Hat, Inc.
@@ -54,7 +54,7 @@ use Vend::File;
 use Vend::Data;
 use Vend::Cron;
 
-$VERSION = substr(q$Revision: 2.204 $, 10);
+$VERSION = substr(q$Revision: 2.205 $, 10);
 
 my %CDname;
 my %CPname;
@@ -598,7 +598,6 @@ sub catalog_directives {
 	['VariableDatabase',  'dbconfig',        ''],
 	['DirConfig',         'dirconfig',        ''],
 	['FileDatabase',	 undef,				 ''],
-	['RequiredFields',   undef,              ''],
 	['NoSearch',         'wildcard',         'userdb'],
 	['OrderCounter',	 undef,     	     ''],
 	['MimeType',         'hash',             ''],
@@ -611,7 +610,6 @@ sub catalog_directives {
 	['ImageDir',	 	 undef,     	     ''],
 	['DeliverImage',     'yesno',			 'no'],
 	['SpecialSub',       'hash',			 ''],
-	['UseCode',		 	 undef,     	     'yes'],
 	['SetGroup',		 'valid_group',      ''],
 	['UseModifier',		 'array',     	     ''],
 	['AutoModifier',	 'array',     	     ''],
@@ -676,7 +674,6 @@ sub catalog_directives {
 	['OrderProfile',	 'profile',     	 ''],
 	['SearchProfile',	 'profile',     	 ''],
 	['OnFly',		 	 undef,     	     ''],
-	['HTMLmirror',		 'yesno',            'No'],
 	['CategoryField',    undef,              'category'],
 	['DescriptionField', undef,              'description'],
 	['PriceDefault',	 undef,              'price'],
