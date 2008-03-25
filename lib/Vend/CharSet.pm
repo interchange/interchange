6c1719d6b73ab541585496ba597427bce2764fab
Kevin Walsh <kevin@cursor.biz>
2008-03-25 10:42:20 +0000

    * Added the Interchange copyright and GPL header comments.

--- lib/Vend/CharSet.pm
+++ lib/Vend/CharSet.pm
@@ -1,3 +1,28 @@
+# Vend::CharSet - utility methods for handling character encoding
+#
+# $Id: CharSet.pm,v 2.2 2008-03-25 10:42:20 kwalsh Exp $
+#
+# Copyright (C) 2002-2008 Interchange Development Group
+# Copyright (C) 1996-2002 Red Hat, Inc.
+#
+# This program was originally based on Vend 0.2 and 0.3
+# Copyright 1995 by Andrew M. Wilcox <amw@wilcoxsolutions.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA  02110-1301  USA.
+#
 package Vend::CharSet;
 
 use strict;
