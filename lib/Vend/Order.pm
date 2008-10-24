4d2069a1f04513ba11e4fa76944a36cda962333a
Peter Ajamian <peter@pajamian.dhs.org>
2008-10-24 10:11:35 +0000

    Fix vulnerability where a string passed in the mv_order_item CGI variable is displayed verbatim without any input sanitation if there is a valid sku in mv_sku.  Thanks to Mat from Bibliopolis for discovering and reporting the vulnerability.

--- lib/Vend/Order.pm
+++ lib/Vend/Order.pm
@@ -1,6 +1,6 @@
 # Vend::Order - Interchange order routing routines
 #
-# $Id: Order.pm,v 2.102 2008-10-03 15:55:40 racke Exp $
+# $Id: Order.pm,v 2.103 2008-10-24 10:11:35 pajamian Exp $
 #
 # Copyright (C) 2002-2008 Interchange Development Group
 # Copyright (C) 1996-2002 Red Hat, Inc.
@@ -29,7 +29,7 @@
 package Vend::Order;
 require Exporter;
 
-$VERSION = substr(q$Revision: 2.102 $, 10);
+$VERSION = substr(q$Revision: 2.103 $, 10);
 
 @ISA = qw(Exporter);
 
@@ -2286,8 +2286,9 @@ sub add_items {
 		for (@sku) {
 			$_ = $::Variable->{MV_VARIANT_JOINER} || '0' if ! length($_);
 		}
-		$skus[0]   = $items[0];
 		$items[0] = join '-', @sku;
+		my $sku_field = $Vend::Cfg->{Options_repository}{Matrix}->{sku} || 'sku';
+		$skus[0] = Vend::Data::product_field($sku_field, $items[0]);
 	}
 
 	if ($Vend::Cfg->{UseModifier}) {
