367e82dcc5b239200f05368aef927deba1f2f6ec
Jon Jensen <jon@endpoint.com>
2009-11-10 14:15:58 -0700

    Increase version to 5.7.4 and add recent changes

--- lib/Vend/Data.pm
+++ lib/Vend/Data.pm
@@ -1,6 +1,6 @@
 # Vend::Data - Interchange databases
 #
-# Copyright (C) 2002-2008 Interchange Development Group
+# Copyright (C) 2002-2009 Interchange Development Group
 # Copyright (C) 1996-2002 Red Hat, Inc.
 #
 # This program was originally based on Vend 0.2 and 0.3
