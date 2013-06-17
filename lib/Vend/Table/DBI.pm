6481828c1e42fda17971201dd63fff9dcd701967
Mike Heins <mike@perusion.com>
2013-06-17 15:11:24 -0400

    * Don't autovivify @fields, even if prior bug fix stopped   problem with @fields == 1.

--- lib/Vend/Table/DBI.pm
+++ lib/Vend/Table/DBI.pm
@@ -1416,6 +1416,9 @@ sub set_row {
 	if ($cfg->{PREFER_NULL}) {
 		for (keys %{$cfg->{PREFER_NULL}}) {
 			my $i = $cfg->{COLUMN_INDEX}{$_};
+
+			## We should not autovivify @fields.
+			next unless exists $fields[$i];
 			undef $fields[$i] if $fields[$i] eq '';
 		}
 	}
