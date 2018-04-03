interchange/interchange

commit fa63936889ba6877869ce9f19c8ac313a8ca375c

Simplify some code (no functional change)

lib/Vend/Data.pm
@@ -1,6 +1,6 @@
 # Vend::Data - Interchange databases
 #
-# Copyright (C) 2002-2017 Interchange Development Group
+# Copyright (C) 2002-2018 Interchange Development Group
 # Copyright (C) 1996-2002 Red Hat, Inc.
 #
 # This program was originally based on Vend 0.2 and 0.3
@@ -2282,11 +2282,11 @@ sub update_data {
 				if $::Pragma->{dml} eq 'strict'
 					|| $function eq 'insert' && $::Pragma->{dml} eq 'preserve';
 
-			for(keys %$qd) {
-#::logDebug("update_data: Getting ready to set_slice");
-				my $k = $multikey ? undef : $key;
-				$qret = $qd->{$_}->set_slice([$dml, $k], $qf->{$_}, $qv->{$_});
-				$rows_set[$i] = $qret unless $rows_set[$i];
+			my $k = $multikey ? undef : $key;
+			my $args = [$dml, $k];
+			for (keys %$qd) {
+				$qret = $qd->{$_}->set_slice($args, $qf->{$_}, $qv->{$_});
+				$rows_set[$i] ||= $qret;
 			}
 			if($blob && $rows_set[$i]) {
 				$brec->{mv_data_fields} = join " ", @fields;

lib/Vend/Table/Editor.pm
@@ -1,6 +1,6 @@
 # Vend::Table::Editor - Swiss-army-knife table editor for Interchange
 #
-# Copyright (C) 2002-2016 Interchange Development Group
+# Copyright (C) 2002-2018 Interchange Development Group
 # Copyright (C) 2002 Mike Heins <mike@perusion.net>
 #
 # This program was originally based on Vend 0.2 and 0.3
@@ -2334,6 +2334,7 @@ show_times("begin table editor call item_id=$key") if $Global::ShowTimes;
 			}
 		}
 	}
+#::logDebug("table editor multikey/key/data=\n" . ::uneval($multikey, $key, $data));
 
 	my $regin = $opt->{all_opts} ? 1 : 0;
 
@@ -2622,13 +2623,8 @@ EOF
 		}
 	}
 
-	my $keycol;
-	if($opt->{notable}) {
-		$keycol = $opt->{ui_data_key_name};
-	}
-	else {
-		$keycol = $opt->{ui_data_key_name} || $db->config('KEY');
-	}
+	my $keycol = $opt->{ui_data_key_name};
+	$keycol ||= $db->config('KEY') if !$opt->{notable};
 
 	###############################################################
 

commit a603f8914031e8611d5cb4b2b3029d6dbc84d463

Fix bug in &Vend::Data::update_data
I inadvertently introduced this bug in commit fa639368 while optimizing
a loop. The arrayref passed to set_slice is, in some cases, apparently
mutated somewhere down the line, though it worked fine in my tests.

Found by Gert van der Spoel <gert@3edge.com>. Thanks, Gert!

lib/Vend/Data.pm
@@ -2288,9 +2288,8 @@ sub update_data {
 					|| $function eq 'insert' && $::Pragma->{dml} eq 'preserve';
 
 			my $k = $multikey ? undef : $key;
-			my $args = [$dml, $k];
 			for (keys %$qd) {
-				$qret = $qd->{$_}->set_slice($args, $qf->{$_}, $qv->{$_});
+				$qret = $qd->{$_}->set_slice([$dml, $k], $qf->{$_}, $qv->{$_});
 				$rows_set[$i] ||= $qret;
 			}
 			if($blob && $rows_set[$i]) {