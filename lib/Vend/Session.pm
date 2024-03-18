ac81644e263e3c83d7675e4196ed9b328e646d76
Andrew Baerg <andrew@pullingshots.ca>
2024-03-18 16:55:06 -0600

    Fix database session locks
    The LOCKLOOP block was missing a while statement which
    according to Perl docs means that it is semantically identical
    to a loop that executes once, and a next statement will
    simply exit the block early.
    So, when a session lock was found, it exited out of the LOCKLOOP
    block, ending up in "# Should never get here", bypassing the lock.
    
    Additionally, the $locktime and $pid variables were not being cleared
    when the lock was cleared, causing the current pid to write a new
    lock and use it to block itself indefinitely.

--- lib/Vend/Session.pm
+++ lib/Vend/Session.pm
@@ -387,11 +387,11 @@ sub lock_session {
 	$name ||= $Vend::SessionName;
 #::logDebug ("lock session id=$Vend::SessionID  name=$Vend::SessionName\n");
 	my $lockname = "LOCK_$name";
-	my ($tried, $locktime, $sleepleft, $pid, $now, $left);
+	my ($tried, $sleepleft, $now, $left);
 	$tried = 0;
 
-	LOCKLOOP: {
-		my $lv;
+	LOCKLOOP: while(1) {
+		my ($lv, $locktime, $pid);
 		if (defined ($lv = $Vend::SessionDBM{$lockname}) ) {
 			($locktime, $pid) = split /:/, $lv, 2;
 		}
